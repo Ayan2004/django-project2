c499888e2480c530e03dd252b7633d8bf03a48cf
Ayan2004 <86516841+Ayan2004@users.noreply.github.com>
2023-05-25 14:08:23 +0530

    Update settings.py

--- project7/settings.py
+++ project7/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55v)fc0wjgb6s(2!m0tu@mepx(uv#74%%j#p*#9lv_@!5n821z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
