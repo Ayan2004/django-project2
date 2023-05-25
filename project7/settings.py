58ceb233d210f19dbfb133d6ee13aa32f6a324de
Ayan2004 <86516841+Ayan2004@users.noreply.github.com>
2023-05-25 12:38:19 +0530

    create superuser

--- project7/settings.py
+++ project7/settings.py
@@ -137,11 +137,11 @@ MEDIA_URL = '/images/'
 #     BASE_DIR / 'static'
 # ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
