3ed612b3368eae91d9fef050ae444053b6ccd8a3
Ayan2004 <86516841+Ayan2004@users.noreply.github.com>
2023-05-25 11:53:13 +0530

    Update settings.py

--- project7/settings.py
+++ project7/settings.py
@@ -137,11 +137,11 @@ MEDIA_URL = '/images/'
 #     BASE_DIR / 'static'
 # ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
